2338be435f878d55aa361e8e224e879570925ab2
heikki.siltala@gmail.com <heikki.siltala@gmail.com@52e2d43a-abb7-d159-3795-db0625f970ed>
2011-12-06 15:12:35 +0000

    Safety commit on widget / imagestrip develoment. Switched to wsetup.html.ep on widget setup controls.

--- trunk/lib/Catz/Data/Widget.pm
+++ trunk/lib/Catz/Data/Widget.pm
@@ -40,20 +40,42 @@ use Catz::Util::Number qw ( round );
 use Catz::Util::String qw ( enurl );
 use Catz::Util::Time qw ( dtlang );
 
+# widget short and long keys as pairs in a single array
+my $wpairs = [ qw ( t type c choose a align f float l limit s size g gap ) ];
+
+# extract short and long keys and
+# create short -> long and long -> short translations
+
+my $wshorts = []; my $wlongs = [];  my $wtrans = {}; 
+
+foreach ( my $i = 0; $i < scalar @{ $wpairs }; $i += 2 ) {
+
+ push @{ $wshorts }, $wpairs->[ $i ];
+ push @{ $wlongs }, $wpairs->[ $i + 1 ];
+
+ $wtrans->{ $wpairs->[ $i ] } = $wpairs->[ $i + 1 ]; 
+ $wtrans->{ $wpairs->[ $i + 1 ] } = $wpairs->[ $i ];
+
+}
+
 # widget setup configuration as a single hashref
 
 my $wconf = {
 
- keys => [ qw ( t d o j g s l ) ],
- defaults => { t => 1, d => 1, o => 1, j => 2, g => 1, s => 150, l => 800 },
+ shorts => $wshorts,
+ longs => $wlongs,
+ trans => $wtrans,
+ 
+ defaults => { t => 1, c => 2, a => 1, f => 1, l => 800, s => 80, g => 0},
+ 
  values => {
-  t => [ 1 ],                                      # type
-  d => [ 1, 2 ],                                   # direction
-  o => [ 1, 2 ],                                   # order
-  j => [ 1, 2, 3 ],                                # justification
-  g => [ 1, 2, 3, 4, 5 ],                          # gaps
-  s => [ map { $_ * 10 } ( 5 .. 20 ) ],            # size
-  l => [ map { 300 + ( $_ * 100 ) } ( 1 .. 17 ) ], # limit
+  type => [ 1 ],
+  choose => [ 1, 2 ],
+  align => [ 1, 2 ],
+  float => [ 1, 2, 3 ],
+  limit => [ map { 300 + ( $_ * 100 ) } ( 1 .. 17 ) ],
+  size => [ map { $_ * 10 } ( 5 .. 20 ) ],
+  gap => [ 0, 2, 4, 6, 8, 10 ],
  },
 
 };
@@ -61,44 +83,43 @@ my $wconf = {
 # prepare the default setup string
 
 my $wdefault = 
- join '', map { $_.$wconf->{defaults}->{$_} } @{ $wconf->{keys} };
- 
-# pre-prepare changes
- 
+ join '', map { $_.$wconf->{defaults}->{$_} } @{ $wconf->{shorts} };
+   
 sub widget_conf { $wconf }
 
 sub widget_verify { 
 
- my $widcon = shift;
+ my $wspec = shift;
    
- my $con = {}; # target
+ my $wrun = {}; # target
    
- length $widcon > 500 and return undef;
+ length $wspec > 500 and return undef;
  
- my @wc = split /([a-z])/, $widcon;
+ my @wc = split /([a-z])/, $wspec;
  
- shift @wc; # the first value is undef
-    
- ( scalar @wc % 2 ) == 0 or return undef;
+ # the first value is undef, remove it
+ shift @wc; 
+ 
+ # must be key-value pairs   
+ ( scalar @wc % 2 ) == 0 or return undef; 
  
  for ( my $i = 0; $i < scalar @wc; $i += 2 ) {
   
-  defined $wc[$i] or return undef;
+  defined $wc[ $i ] or return undef;
+  defined $wc[ $i+1 ] or return undef;
   
-  defined $wc[$i+1] or return undef;
-  
-  exists $wconf->{values}->{$wc[$i]} or return undef;
+  ( my $key = $wconf->{trans}->{ $wc[ $i ] } // undef ) or return undef;
  
-  ( any { $_ eq $wc[$i+1] } @{ $wconf->{values}->{$wc[$i]} } )
+  ( any { $_ eq $wc[ $i + 1 ] } @{ $wconf->{values}->{$key} } )
    or return undef;
      
-  $con->{$wc[$i]} = $wc[$i+1];
+  $wrun->{$key} = $wc[ $i + 1 ];
    
  }
 
- do { exists $con->{$_} or return undef } foreach @{ $wconf->{keys} };
- 
- return $con;
+ do { exists $wrun->{ $_ } or return undef } foreach @{ $wconf->{longs} };
+  
+ return $wrun;
  
 }
 
